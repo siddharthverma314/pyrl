2780e433b9111a2acc8042a66a82241892d53d92
Siddharth Verma <siddharthverma314@gmail.com>
2020-07-21 02:58:04 -0700

    Bugs with torchify

--- pyrl/utils/torchify.py
+++ pyrl/utils/torchify.py
@@ -1,11 +1,21 @@
 import torch
+import numbers
+import numpy as np
 
 
 def torchify(obs, device="cpu"):
     if isinstance(obs, dict):
-        return {k: torchify(v) for k, v in obs.items()}
-    if not isinstance(obs, torch.Tensor):
+        ret = {}
+        for k, v in obs.items():
+            if (tv := torchify(v, device)) is not None:
+                ret[k] = tv
+        return ret
+    if isinstance(obs, np.ndarray):
         obs = torch.tensor(obs).float()
+    elif isinstance(obs, numbers.Number):
+        obs = torch.tensor(obs).float()
+    elif not isinstance(obs, torch.Tensor):
+        return None
     while obs.dim() < 2:
         obs = obs.unsqueeze(0)
     return obs.to(device)
