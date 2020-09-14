832c2f433584810407406231832023e3447050f2
Siddharth Verma <siddharthverma314@gmail.com>
2020-09-13 19:11:20 -0700

    Added niv

--- default.nix
+++ default.nix
@@ -1,13 +1,22 @@
 { pkgs ? import ./nix/nixpkgs.nix {} }:
+let
+  niv = (pkgs.callPackage pkgs.sources.niv {}).niv;
+in
 {
   pkg = pkgs.python3Packages.callPackage ./derivation.nix {};
-  dev = pkgs.python3.withPackages (ps: with ps; [
-    pkgs.python-language-server
-    pyls-black
-    ipdb
-    rope
-    pyflakes
-    pytest
-    tensorflow-tensorboard
-  ]);
+  dev = pkgs.buildEnv {
+    name = "pyrl-dev";
+    paths = [
+      niv
+      pkgs.python-language-server
+      (pkgs.python3.withPackages (ps: with ps; [
+        pyls-black
+        ipdb
+        rope
+        pyflakes
+        pytest
+        tensorflow-tensorboard
+      ]))
+    ];
+  };
 }
