a1c17cd398e5959cc0e62331b879f342cf888b6c
Siddharth Verma <siddharthverma314@gmail.com>
2020-11-14 01:38:41 -0800

    Removed each specific devshell

--- flake.nix
+++ flake.nix
@@ -1,33 +1,12 @@
 {
   inputs = {
-    cpprb = {
-      url = github:ymd-h/cpprb;
-      flake = false;
-    };
-    flatten-dict = {
-      url = github:ianlini/flatten-dict;
-      flake = false;
-    };
-    gitignore = {
-      url = github:hercules-ci/gitignore;
-      flake = false;
-    };
-    mujoco = {
-      url = https://www.roboti.us/download/mjpro150_linux.zip;
-      flake = false;
-    };
-    mujoco-py = {
-      url = github:siddharthverma314/mujoco-py;
-      flake = false;
-    };
-    pyGLFW = {
-      url = github:FlorianRhiem/pyGLFW;
-      flake = false;
-    };
-    scikit-video = {
-      url = github:scikit-video/scikit-video;
-      flake = false;
-    };
+    cpprb = { url = github:ymd-h/cpprb; flake = false; };
+    flatten-dict = { url = github:ianlini/flatten-dict; flake = false; };
+    gitignore = { url = github:hercules-ci/gitignore; flake = false; };
+    mujoco = { url = https://www.roboti.us/download/mjpro150_linux.zip; flake = false; };
+    mujoco-py = { url = github:siddharthverma314/mujoco-py; flake = false; };
+    pyGLFW = { url = github:FlorianRhiem/pyGLFW; flake = false; };
+    scikit-video = { url = github:scikit-video/scikit-video; flake = false; };
     nixpkgs.url = github:NixOS/nixpkgs;
   };
   outputs = inputs: let
@@ -37,15 +16,17 @@
         cudaSupport = false;
         python = "python38";
       };
-    in
-      pkgs.python3Packages.callPackage ./derivation.nix {};
-
-    pkgs_cpu_py37 = mkPkg false "python37";
-    pkgs_gpu_py37 = mkPkg true "python37";
-    pkgs_cpu_py38 = mkPkg false "python38";
-    pkgs_gpu_py38 = mkPkg true "python38";
+      pkg = pkgs.python3Packages.callPackage ./derivation.nix {};
+    in { inherit pkg; dev = import ./shell.nix {inherit pkg pkgs; }; };
+    packages = {
+      py37_cpu = mkPkg false "python37";
+      py37_gpu = mkPkg true "python37";
+      py38_cpu = mkPkg false "python38";
+      py38_gpu = mkPkg true "python38";
+    };
   in {
-    packages.x86_64-linux = { inherit pkgs_cpu_py37 pkgs_cpu_py38 pkgs_gpu_py37 pkgs_gpu_py38; };
-    defaultPackage.x86_64-linux = pkgs_gpu_py38;
+    packages.x86_64-linux = builtins.mapAttrs (_: v: v.pkg) packages;
+    defaultPackage.x86_64-linux = packages.py38_gpu.pkg;
+    devShell.x86_64-linux = packages.py38_gpu.dev;
   };
 }
--- shell.nix
+++ shell.nix
@@ -1,26 +1,16 @@
-{ pkgs, pyrl }:
-let
-  dev = pkgs.buildEnv {
-    name = "pyrl-dev";
-    paths = [
-      pkgs.python-language-server
-      (pkgs.python3.withPackages (ps: with ps; [
-        pyrl
-        pyls-black
-        ipdb
-        rope
-        pyflakes
-        pytest
-        tensorflow-tensorboard_2
-      ]))
-    ];
-  };
-in
-pkgs.mkShell {
-  buildInputs = pyrl.pkg.propagatedBuildInputs ++ [
-    pyrl.dev
+{ pkgs, pkg }:
+pkgs.buildEnv {
+  name = "pyrl-dev";
+  paths = [
+    pkgs.python-language-server
+    (pkgs.python3.withPackages (ps: with ps; [
+      pkg
+      pyls-black
+      ipdb
+      rope
+      pyflakes
+      pytest
+      tensorflow-tensorboard_2
+    ]))
   ];
-  shellHook = ''
-    export PYTHONPATH=$PYTHONPATH:$PWD
-  '';
 }
